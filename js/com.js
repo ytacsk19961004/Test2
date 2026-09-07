d7923091a9ef8133947b6a4277e79675f61db7e4
ytacsk19961004 <35586195+ytacsk19961004@users.noreply.github.com>
2026-09-07 13:09:17 +0900

    download filename changed

--- js/com.js
+++ js/com.js
@@ -204,9 +204,20 @@ function downloadLog() {
     // ダウンロード対象のテキストを取得
     const logText  = LogContainer.textContent
     // ダウンロード：ファイル名
-    const fileName = "test.txt"
+    const date = new Date()
+
+    
+    
+    
+    
+    
+    
+    
+
+    const fileName = `計量記録_${date.getFullYear()}${date.getMonth()}${date.getDate()}${date.getHours()}${date.getMinutes()}${date.getSeconds()}.csv`
     // ダウンロード：ファイル内容
-    const fileBlob = new Blob([logText], { type: `${"text/plain"};charset=utf-8;` }) 
+    // const fileBlob = new Blob([logText], { type: `${"text/plain"};charset=utf-8;` }) 
+    const fileBlob = new Blob([logText], { type: `${"text/csv"};charset=utf-8;` }) 
     // 一時的なダウンロード用リンクの作成
     const link = document.createElement('a')
     link.href = URL.createObjectURL(fileBlob)
